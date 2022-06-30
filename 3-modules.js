ba1bc96f0465f13315c1e7e37c61d5c1e0e6436a
Sumanto Banerjee <sumanto.banerjee08@Sumantos-MacBook-Pro-2.local>
2022-06-30 11:54:46 +0530

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alt-flavor')
+require('./7-mind-grenade')
+console.log(names);
+
+sayHi('Nancy')
+sayHi(names.susan)
+sayHi(names.peter)
---
+++ 4-names.js
@@ -0,0 +1,9 @@
+// local
+const secret = 'super secret'
+// share
+const susan = 'Susan'
+const peter = 'Peter'
+
+
+module.exports = {susan, peter}
+// use objects when exporting multiple items
